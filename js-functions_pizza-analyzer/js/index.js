e62fbb35b354d10bdb34bcf1e8ca9f5559f8ddf8
Richard Beindorf <richard_beindorf@web.de>
2024-03-25 11:07:50 +0100

    pizza challenge done

--- js-functions_pizza-analyzer/js/index.js
+++ js-functions_pizza-analyzer/js/index.js
@@ -9,18 +9,28 @@ const outputSection = document.querySelector('[data-js="output-section"]');
 const output = document.querySelector('[data-js="output"]');
 
 pizzaInput1.addEventListener("input", () => {
-  // write your code here
+  let pizzaSize1 = parseInt(pizzaInput1.value);
+  let pizzaSize2 = parseInt(pizzaInput2.value);
+  calculatePizzaGain(pizzaSize1, pizzaSize2);
 });
 
 pizzaInput2.addEventListener("input", () => {
-  // write your code here
+  let pizzaSize1 = parseInt(pizzaInput1.value);
+  let pizzaSize2 = parseInt(pizzaInput2.value);
+    calculatePizzaGain(pizzaSize1, pizzaSize2);
+  
 });
 
 // Task 1
 
-function calculatePizzaGain() {
-  
+function calculatePizzaGain(diameter1, diameter2) {
+  const area1 = Math.PI * diameter1;
+  const area2 = Math.PI * diameter2;
+  const gainOne = (area2 - area1) / area1 * 100;
+  return outputSection.textContent = Math.round(gainOne)+"%";
 }
+
+// calculatePizzaGain(pizzaSize1, pizzaSize2);
 
 // Task 2
 // define the function updatePizzaDisplay here
