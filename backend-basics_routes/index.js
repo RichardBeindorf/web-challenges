7c04a11ecfb957cb5a673cdab2a514cc9a90d86a
Richard Beindorf <richard_beindorf@web.de>
2024-04-16 16:25:29 +0200

    second challenge done

---
+++ backend-basics_routes/index.js
@@ -0,0 +1,6 @@
+// import { server } from "./server.js";
+
+// server.listen(…
+import { server } from "./server.js"
+
+server.listen(8000, () => {console.log("Server is hostet on http://localhost:8000")})
